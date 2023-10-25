6a420c6d8258ecde34c44666cd214f2ac0403753
John Cummins <jcummins@c2r5s2.42prague.com>
2023-10-25 12:39:11 +0200

    Updated atoi to handle single + or - sign

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,30 +6,33 @@
 /*   By: jcummins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 19:03:56 by jcummins          #+#    #+#             */
-/*   Updated: 2023/10/23 19:05:52 by jcummins         ###   ########.fr       */
+/*   Updated: 2023/10/25 12:38:24 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
 	unsigned int	i;
 	unsigned int	result;
+	int				sign;
 
 	i = 0;
 	result = 0;
-	while (nptr[i])
+	sign = 1;
+	if (nptr[i] == '-' || nptr[i] == '+')
+		sign = (44 - nptr[i++]);
+	while (nptr[i] && ((nptr[i] >= '0') && nptr[i] <= '9'))
 		result = (result * 10) + (nptr[i++] - '0');
-	return (result);
+	return (result * sign);
 }
 
 int	main(void)
 {
-	const char	*message = "12343";
-	printf("The string is %s and the integer value is %d\n", message, ft_atoi(message));
+	const char	*message = "-12343g";
+
+	printf("String is %s, integer is %d\n", message, ft_atoi(message));
 	return (0);
 }
-/*DESCRIPTION*/
-/*The atoi() function converts the initial portion of the string pointed to by nptr to int. The behaviour is the same as strtol except that atoi() does not detect errors.*/
